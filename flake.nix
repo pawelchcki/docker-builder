13206084e12b908b69aee59eb524933e10e18f97
Pawel Chojnacki <pawelchcki@gmail.com>
2024-02-05 17:45:08 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -1,47 +1,58 @@
 {
-  description = "KTG";
-  nixConfig.bash-prompt-prefix = "\[ktg\] ";
+  description = "docker-builder";
+  nixConfig.bash-prompt-prefix = "\[d-b\] ";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    pyproject-nix.url = "github:nix-community/pyproject.nix";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    treefmt-nix,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , treefmt-nix
+    , pyproject-nix
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      project = pyproject-nix.lib.project.loadPyproject {
+        projectRoot = ./.;
+      };
       pkgs = nixpkgs.legacyPackages.${system};
       python = pkgs.python310;
+
+      projectAttrs = project.renderers.buildPythonPackage { inherit python; };
+
+      projectPythonDeps = project.renderers.withPackages { inherit python; };
+
+      pythonEnv = python.withPackages projectPythonDeps;
+
+
       pythonPackages = pkgs.python310Packages;
       treefmt = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-      devtools = pkgs.symlinkJoin {
-        name = "devtools";
-        paths = [
-          python
-          pkgs.curl
-        ];
-        postBuild = "echo links added";
-      };
-    in {
+    in
+    {
       packages = {
         python = python;
-        default = python;
-        devtools = devtools;
       };
+
+      packages.default = python.pkgs.buildPythonApplication ( projectAttrs );
+
       formatter = treefmt.config.build.wrapper;
       devShells.default =
         pkgs.mkShell
-        {
-          venvDir = "./.venv";
-          packages =
-            [
-              devtools
-            ]
-            ++ (with pythonPackages; [six setuptools wheel venvShellHook]);
-        };
+          {
+            venvDir = "./.venv";
+            packages =
+              [
+                pythonEnv
+                pythonPackages.pytest
+                pythonPackages.venvShellHook
+              ];
+          };
     });
 }
